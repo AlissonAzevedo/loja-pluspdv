a63548400c9aa28c8d946c58937d27d039c79f58
Alisson Azevedo <alissonazevedo13.1@gmail.com>
2022-10-10 08:46:09 -0300

    chore: rename button login and register

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -5,12 +5,12 @@
         <div class="w-full flex flex-col justify-center items-center">
             <router-link to="/conta/entrar" class="w-full flex flex-col justify-center items-center mt-8">
                 <h3 class="text-xs text-white bg-[#E74845] py-4 w-full text-center rounded">
-                    Login
+                    Já sou cliente
                 </h3>
             </router-link>
             <router-link to="/conta/cadastro" class="w-full flex flex-col justify-center items-center mt-8 text-center">
                 <h3 class="text-xs text-white py-4 w-full border-2 border-white rounded">
-                    Cadastro
+                    Criar nova conta
                 </h3>
             </router-link>
         </div>
