41a73bc726f14c314a4c837ee99ddc8907fb1e75
Alisson Azevedo <alissonazevedo13.1@gmail.com>
2022-10-11 10:40:39 -0300

    Revert "Update vue.config.js"
    This reverts commit 8a76caa42aaa0865b2f9a2bb15005661f64cf86d.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  proxy: 'http://159.89.176.78:8001'
+  transpileDependencies: true
 })
